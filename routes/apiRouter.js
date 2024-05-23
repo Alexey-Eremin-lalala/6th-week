47f08ac7dbfcc485db91c2719a89b9b5d30ab655
Alexey-Eremin-lalala <149609252+Alexey-Eremin-lalala@users.noreply.github.com>
2024-05-23 20:05:20 +0300

    коллекции для mongodb

---
+++ routes/apiRouter.js
@@ -0,0 +1,13 @@
+const categoriesRouter = require("./categories");
+const gamesRouter = require("./games");
+const usersRouter = require("./users");
+const apiRouter = require("express").Router(); 
+const authRouter = require('./auth')
+
+
+apiRouter.use("/api", gamesRouter); 
+apiRouter.use("/api", usersRouter);
+apiRouter.use("/api", categoriesRouter);
+apiRouter.use("/api", authRouter);
+
+module.exports = apiRouter; 
